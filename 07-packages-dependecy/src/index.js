9a397e8580abd0f619c26b26f0a58c2e6e4d2b66
Augustus100 <augustusrossi14@gmail.com>
2026-04-12 08:52:11 -0300

    feat: conclusão de um novo módulo

---
+++ 07-packages-dependecy/src/index.js
@@ -0,0 +1,14 @@
+import chalck from "chalk";
+import logSymbols from "log-symbols";
+
+console.log(chalck.blue.bgGray.italic("Prática de Package"));
+
+console.log(logSymbols.success, chalck.green.bgMagenta("servidor funcionando"));
+
+
+//para desinstalar um pacote  usa o npm unistall nome-do-pacote
+
+//npm fund: 
+//  busca todos os links de dowload usados 
+
+//atualizar um pacote: npm update nome-do-pacote
